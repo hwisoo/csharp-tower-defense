7ef582e95a2d13c5b443e181cfeda9077ed213d3
James Cho <hwisoocho@gmail.com>
2018-12-15 22:05:10 -0800

    FireOnInvaders()

--- Tower.cs
+++ Tower.cs
@@ -2,6 +2,19 @@ namespace Defense
 {
   class Tower
   {
-    
+    private readonly MapLocation _location;
+
+    public Tower(MapLocation location)
+    {
+      _location = location;
+    }
+
+    public void FireOnInvaders(Invader[] invaders)
+    {
+      for(int i =0; i < invaders.Length; i++)
+      {
+        Invader invader = invaders[i];
+      }
+    }
   }
 }
